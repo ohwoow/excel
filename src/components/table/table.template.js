45ad0d3d483390af25ac204a2f245013b61f36e6
Даня <diman100l@mail.ru>
2022-06-15 13:53:19 +0500

    finish table resize

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -5,22 +5,30 @@ const CODES = {
   Z: 90
 }
 
-function createCell() {
+function createCell(_, index) {
   return `
-    <div class="cell" contenteditable></div>
+    <div class="cell" data-cell="${index}" contenteditable></div>
   `
 }
 
-function toColumn(col) {
+function toColumn(col, index) {
   return `
-    <div class="column">${col}</div>
+    <div class="column" data-type="resizable" data-col="${index}">
+      ${col}
+      <div class="col-resize" data-resize="col"></div>
+    </div>
   `
 }
+
 
 function createRow(content = '', number = '') {
+  const resize = number ? '<div class="row-resize" data-resize="row"></div>' : ''
   return `
-    <div class="row">
-      <div class="row-info">${number}</div>
+    <div class="row" data-type="resizable">
+      <div class="row-info" >
+        ${number}
+        ${resize}
+      </div>
       <div class="row-data">${content}</div>
     </div>
   `
@@ -34,14 +42,13 @@ export function createTable(rowsCount = 10) {
   const colsCount = CODES.Z - CODES.A + 1
   const rows = []
 
-  const cells = new Array(colsCount).fill(createCell()).join('')
+  const cells = new Array(colsCount).fill('').map(createCell).join('')
 
   const cols = new Array(colsCount)
       .fill('')
       .map(toChar)
       .map(toColumn)
       .join('')
-
 
   rows.push(createRow(cols))
 
